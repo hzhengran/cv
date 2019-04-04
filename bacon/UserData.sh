6d72478abc868d1fd60334adae742561d8650b6e
Joseph Chenkie <josephchenjian@icloud.com>
2019-04-04 19:35:38 +0800

    Update UserData.sh

--- bacon/UserData.sh
+++ bacon/UserData.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 ## install dependencies -- swift vapor
 sudo apt install -y update
-sudo apt-get -y install clang curl
-sudo apt-get -y install libcurl3 libpython2.7 libpython2.7-dev
+sudo apt-get install -y clang curl
+sudo apt-get install -y libcurl3 libpython2.7 libpython2.7-dev
 wget https://swift.org/builds/swift-4.2.1-release/ubuntu1804/swift-4.2.1-RELEASE/swift-4.2.1-RELEASE-ubuntu18.04.tar.gz
 tar xzf swift-4.2.1-RELEASE-ubuntu18.04.tar.gz
 sudo mv swift-4.2.1-RELEASE-ubuntu18.04 /usr/share/swift
 echo "export PATH=/usr/share/swift/usr/bin:$PATH" >> ~/.bashrc
 source  ~/.bashrc
 eval "$(curl -sL https://apt.vapor.sh)"
-sudo apt-get install swift vapor
+sudo apt-get install -y swift vapor
 swift --version
 vapor --help
 
