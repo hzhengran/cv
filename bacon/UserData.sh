83d46f3faafd03b37ae1456bbdf483a02cdf16dc
Joseph Chenkie <josephchenjian@icloud.com>
2019-04-04 20:15:57 +0800

    Update UserData.sh

--- bacon/UserData.sh
+++ bacon/UserData.sh
@@ -1,17 +1,21 @@
 #!/bin/bash
 ## install dependencies -- swift vapor
-sudo apt install -y update
+mkdir a
+sudo apt-get update
 sudo apt-get install -y clang curl
 sudo apt-get install -y libcurl3 libpython2.7 libpython2.7-dev
+mkdir b
 wget https://swift.org/builds/swift-4.2.1-release/ubuntu1804/swift-4.2.1-RELEASE/swift-4.2.1-RELEASE-ubuntu18.04.tar.gz
 tar xzf swift-4.2.1-RELEASE-ubuntu18.04.tar.gz
 sudo mv swift-4.2.1-RELEASE-ubuntu18.04 /usr/share/swift
+mkdir chenjianAgain
 echo "export PATH=/usr/share/swift/usr/bin:$PATH" >> ~/.bashrc
 source  ~/.bashrc
+mkdir d
 eval "$(curl -sL https://apt.vapor.sh)"
 sudo apt-get install -y swift vapor
-swift --version
-vapor --help
+##swift --version
+##vapor --help
 
 ## download source code from github
 wget https://raw.githubusercontent.com/chenjianAgain/cv/master/bacon/key.pri
@@ -23,4 +27,5 @@ git clone git@github.com:derekcoder/AcronymApp.git && cd AcronymApp
 sed -i 's/localhost/35.238.101.198/g' Sources/App/configure.swift
 
 ## vapor build and run in background
-vapor build && nohup vapor run --hostname=0.0.0.0 --port=8080 &
+vapor build 
+nohup vapor run --hostname=0.0.0.0 --port=8080 &
