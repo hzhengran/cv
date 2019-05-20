65e269f7afc66438d1eec60050f2c3df3beb1fe4
Joseph Chenkie <josephchenjian@icloud.com>
2019-05-20 16:36:37 +0800

    Update test.sh

--- bacon/test.sh
+++ bacon/test.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 start=`date +"%s"`
-for (( i=0; i<1; i++ ))
+for (( i=0; i<10; i++ ))
 do
     {
-        echo "multiprocess"
+       echo "multiprocess"
        aws s3 cp s3://bdm-workshop/informatica_1022_server_linux-x64.tar . --region cn-north-1 
-       # sleep 3
+       sleep 3
+       rm -rf info*
     } &  
 done
 wait    
