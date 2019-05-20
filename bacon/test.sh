c492d8e15c83dffb35d5c3e776529dac98853855
Joseph Chenkie <josephchenjian@icloud.com>
2019-05-20 15:26:09 +0800

    Create test.sh

---
+++ bacon/test.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+start=`date +"%s"`
+for (( i=0; i<10; i++ ))
+do
+    {
+        echo "multiprocess"
+       aws s3 cp s3://bdm-workshop/informatica_1022_server_linux-x64.tar . --region cn-north-1 
+       # sleep 3
+    } &  
+done
+wait    
+end=`date +"%s"`
+echo "time: " `expr $end - $start`
+
+rm -rf info*
