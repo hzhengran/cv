662e0681b858d761bfc5cf7654e48154e96d816a
Joseph Chenkie <josephchenjian@icloud.com>
2019-05-21 05:58:56 +0800

    Update test.sh

--- bacon/test.sh
+++ bacon/test.sh
@@ -5,13 +5,13 @@ touch $FILE
 
 start=`date +"%s"`
 echo "`date +%F` `date +%T`: Begin" >> $FILE
-#for (( i=0; i<4; i++ ))
-for i in $(seq 2) 
+for (( i=0; i<4; i++ ))
+# for i in $(seq 2) 
 do
     {
        echo "multiprocess"
        aws s3 cp s3://bdm-workshop/informatica_1022_server_linux-x64.tar . --region cn-north-1 
-       sleep 300
+       # sleep 300
        # rm -rf info*
        wait
     } # &  
