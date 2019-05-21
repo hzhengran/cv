c6ee88908d2d24110301d6c4792eb30e6dd42347
Joseph Chenkie <josephchenjian@icloud.com>
2019-05-21 14:19:44 +0800

    Update deadLoop.sh

--- bacon/deadLoop.sh
+++ bacon/deadLoop.sh
@@ -5,10 +5,13 @@ touch $FILE
 
 start=`date +"%s"`
 echo "`date +%F` `date +%T`: Begin" >> $FILE
+
+i=0
 while : 
 do
     {
        echo "multiprocess"
+       let i++
        echo "`date +%F` `date +%T`: $i time begins" >> $FILE
        aws s3 cp s3://bdm-workshop/informatica_1022_server_linux-x64.tar . --region cn-north-1 
        echo "`date +%F` `date +%T`: $i time completed" >> $FILE
