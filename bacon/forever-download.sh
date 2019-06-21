959349dc4217dffa78f6ac42c286b67d92c9af8d
Joseph Chenkie <josephchenjian@icloud.com>
2019-06-21 17:21:51 +0800

    Update forever-download.sh

--- bacon/forever-download.sh
+++ bacon/forever-download.sh
@@ -2,6 +2,6 @@
 while [ 1 ]
 do
       sleep 1
-      rm -rf *.rar
+      rm -rf Win*
       wget "https://software-download.microsoft.com/pr/Win10_1903_V1_EnglishInternational_x32.iso?t=c6364759-7e93-4a19-871b-380ae23878c8&e=1561193341&h=01e58fd22bb5895396d7d757680b2143"
 done
